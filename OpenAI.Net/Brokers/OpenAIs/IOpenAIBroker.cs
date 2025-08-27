4b0be6d3565402d55f3f222adf6e67f943a8af66
John <benjamin@mitochronhub.com>
2025-08-28 00:47:06 +0100

    BROKERS: Initialize API Broker
    #9

---
+++ OpenAI.Net/Brokers/OpenAIs/IOpenAIBroker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAI.Net.Brokers.OpenAIs
+{
+    internal partial interface IOpenAIBroker
+    {
+    }
+}
