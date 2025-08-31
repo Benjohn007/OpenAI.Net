c966eb905e736886a14e39ff259be2004a00450f
John <benjamin@mitochronhub.com>
2025-08-31 22:37:10 +0100

    Broker: Initialize Completion Data

---
+++ OpenAI.Net/Models/ExternalCompleteions/Choice.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace OpenAI.Net.Models.ExternalCompleteions
+{
+    internal partial class CompletionResponse
+    {
+        public class Choice
+        {
+            [JsonProperty("text")]           
+            public string Text { get; set; }
+
+            [JsonProperty("index")]          
+            public int Index { get; set; }
+
+            [JsonProperty( "logprobs")]
+            public object Logprobabilities { get; set; }
+
+            [JsonProperty("finish_reason")]           
+            public string FinishReason { get; set; }
+        }
+
+    }
+}
---
+++ OpenAI.Net/Models/ExternalCompleteions/Usage.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace OpenAI.Net.Models.ExternalCompleteions
+{
+    internal partial class CompletionResponse
+    {
+        public class Usage
+        {
+            [JsonProperty("prompt_tokens")]         
+            public int PromptTokens { get; set; }
+
+            [JsonProperty("completion_tokens")]
+            public int CompletionTokens { get; set; }
+
+            [JsonProperty("total_tokens")]
+            public int TotalTokens { get; set; }
+        }
+
+    }
+}
